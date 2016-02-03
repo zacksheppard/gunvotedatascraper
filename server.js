5692834680b8d332024dbef4dca667661d0fac1e
Zack Sheppard <z@zack.io>
2016-02-03 09:14:11 -0500

    initial scrape test

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 
 // https://github.com/lapwinglabs/x-ray
-var Xray = require('x-ray'); 
+var xray = require('x-ray'); 
+var x = xray();
 
 // also available: https://github.com/lapwinglabs/x-ray-phantom
 // var phantom = require('x-ray-phantom'); 
+
+var url = "https://votesmart.org/interest-group/122/rating/5070#.VrIFPmQrK2x";
+
+x(url, {
+  name: 'td:nth-child(4)',
+  score: 'td:nth-child(6)'
+})(function(err, td) {
+  if(err) {
+    console.log('Error' + err)
+  } else {
+  console.log(td);
+  //console.log('TEST');
+  }
+});
