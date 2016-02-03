c108b2c8d4606263fbcbb9618efd19dbabc6b6e6
Zack Sheppard <z@zack.io>
2016-02-03 08:45:58 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+
+// https://github.com/lapwinglabs/x-ray
+var Xray = require('x-ray'); 
+
+// also available: https://github.com/lapwinglabs/x-ray-phantom
+// var phantom = require('x-ray-phantom'); 
